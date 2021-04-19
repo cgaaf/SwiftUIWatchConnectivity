ac1e29998178ae468be89b14a7d0a1732a39bd1d
cgaafary <cgaafary@gmail.com>
2021-04-19 00:01:50 -0400

    Initial Commit

---
+++ SwiftUIWatchConnectivity WatchKit Extension/SwiftUIWatchConnectivityApp.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIWatchConnectivityApp.swift
+//  SwiftUIWatchConnectivity WatchKit Extension
+//
+//  Created by Chris Gaafary on 4/19/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWatchConnectivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
---
+++ SwiftUIWatchConnectivity/SwiftUIWatchConnectivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWatchConnectivityApp.swift
+//  SwiftUIWatchConnectivity
+//
+//  Created by Chris Gaafary on 4/19/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWatchConnectivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
